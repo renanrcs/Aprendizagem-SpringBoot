6564afd40a7a63f912af0593e37364c28b3f11d1
renanrcs <renan.cavalcante@yahoo.com>
2023-05-31 14:21:58 -0300

    Initial commit

---
+++ src/main/java/io/github/renanrcs/api/AprendizagemApplication.java
@@ -0,0 +1,13 @@
+package io.github.renanrcs.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprendizagemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AprendizagemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/renanrcs/api/AprendizagemApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.renanrcs.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AprendizagemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
